08d792027b9f1a43f15ab777b50fdc0ff7e4a9a5
ellenchart <ellen.hart@richmond.edu>
2024-06-12 14:04:09 -0400

    up to 44

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -1,36 +1,19 @@
 package stepDefinitions;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
+import static driver.DriverFactory.getDriver;
+
 public class Contact_Us_Steps {
-    private WebDriver driver;
-
-    @Before
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        driver = new ChromeDriver(chromeOptions);
-        driver.manage().window().maximize();
-    }
-
-    @After
-    public void tearDown() {
-      driver.quit();
-    }
+    private WebDriver driver = getDriver();
 
     public String generateRandomNumber(int length) {
         return RandomStringUtils.randomNumeric(length);
@@ -63,6 +46,26 @@ public class Contact_Us_Steps {
     @And("I enter a unique comment")
     public void i_enter_a_unique_comment() {
         driver.findElement(By.xpath("//textarea[@name=\"message\"]")).sendKeys("Hello world" + generateRandomString(20));
+    }
+
+    @When("I enter a specific first name {word}")
+    public void i_enter_a_specific_first_name(String firstName) {
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys(firstName);
+    }
+
+    @When("I enter a specific last name {word}")
+    public void i_enter_a_specific_last_name(String lastName) {
+        driver.findElement(By.xpath("//input[@name=\"last_name\"]")).sendKeys(lastName);
+    }
+
+    @When("I enter a specific email address {word}")
+    public void i_enter_a_specific_email_address(String email) {
+        driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys(email);
+    }
+
+    @When("I enter a specific comment {string}")
+    public void i_enter_a_specific_comment(String comment) {
+        driver.findElement(By.xpath("//textarea[@name=\"message\"]")).sendKeys(comment);
     }
 
     @And("I click on the submit button")
--- src/test/resources/features/Contact_Us.feature
+++ src/test/resources/features/Contact_Us.feature
@@ -1,7 +1,11 @@
+@contact-us @regression
+
 Feature: WebDriver University - Contact Us Page
 
-  Scenario: Validate Successful Submission - Unique Data
+  Background:
     Given I access the webdriver university contact us page
+
+  Scenario: Validate Successful Submission - Unique Data
     When I enter a unique first name
     And I enter a unique last name
     And I enter a unique email address
@@ -10,3 +14,9 @@ Feature: WebDriver University - Contact Us Page
     Then I should be presented with a successful contact us submission message
 
   Scenario: Validate Successful Submission - Specific Data
+    When I enter a specific first name joe
+    And I enter a specific last name blogs
+    And I enter a specific email address joe_blogs123@mail.com
+    And I enter a specific comment "How are you?"
+    And I click on the submit button
+    Then I should be presented with a successful contact us submission message
